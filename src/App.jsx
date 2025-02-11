fbf4e9ba8ce1a728b5aeac5ecdb6c6c3a55b3cd9
KavinduJayawardhana <kavindudeshan1018@gmail.com>
2025-02-11 12:00:00 +0530

    responsive navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="text-3xl font-bold underline">
-        Hello World
+      <div className="">
+        <Header/>
       </div>
     </>
   )
